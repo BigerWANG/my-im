1d1e01fdeb73cf60f3518b4fc1604930ecb553c6
wangmengyu <wangmengyu@yupiao.com>
2021-03-19 00:16:50 +0800

    添加用户广播功能

--- server/server.go
+++ server/server.go
@@ -3,21 +3,29 @@ package server
 import (
 	"fmt"
 	"log"
+	"my-im/user"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	IP string
 	Port int
+
+	OnlineMap map[string]*user.User
+	Message chan string
+	mu sync.RWMutex
 }
 
-type OnlineMap map[string]string
 
 
 func NewServer(ip string, port int) *Server {
 	return  &Server{
 		IP:   ip,
 		Port: port,
+		OnlineMap: make(map[string]*user.User),
+		Message: make(chan string),
+
 	}
 }
 
@@ -28,6 +36,9 @@ func(s *Server) Start(){
 	if  err != nil{
 		log.Fatal()
 	}
+	// 启动监听message的go
+	go s.ListenMessager()
+
 
 	defer func() {
 		err = listener.Close()
@@ -41,16 +52,39 @@ func(s *Server) Start(){
 			}
 			continue
 		}
-		go Handler(conn)
+		go s.Handler(conn)
 	}
 
 }
 
-func Handler(conn net.Conn) {
-	fmt.Println("当前连接建立成功")
-	content, err := conn.Read([]byte{100})
-	if err != nil{
-		fmt.Println(err)
+func(s *Server) Handler(conn net.Conn) {
+	u := user.NewUser(conn)
+	// 启动用户监听程序
+	go u.ListenMessage()
+	s.mu.Lock()
+	// 将当前用户保存在map中
+	s.OnlineMap[u.Name]=u
+	s.mu.Unlock()
+	// 广播当前用户上线消息
+	s.BoradCast(u, "已上线")
+}
+
+// 广播消息
+func(s *Server)BoradCast(u *user.User, msg string){
+	sendMsg := fmt.Sprintf("[%s]%s: %s", u.Addr, u.Name, msg)
+	s.Message <- sendMsg
+}
+
+// 监听Message广播消息channnel的goroutine,一旦有消息就发送给全部在线的user
+func (s *Server)ListenMessager()  {
+	for {
+		msg := <- s.Message
+
+		s.mu.Lock()
+		for _, cli := range s.OnlineMap{
+			cli.UserChan <- msg
+		}
+		s.mu.Unlock()
 	}
-	fmt.Println(content)
+	
 }
--- user/user.go
+++ user/user.go
@@ -1,6 +1,9 @@
 package user
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type User struct {
 	Name  string
@@ -9,3 +12,27 @@ type User struct {
 	conn net.Conn
 }
 
+
+func NewUser(conn net.Conn)*User{
+
+	return &User{
+		Name:     conn.RemoteAddr().String(),
+		Addr:     conn.RemoteAddr().String(),
+		UserChan: make(chan string),
+		conn:     conn,
+	}
+
+}
+
+// 监听当前User channel的方法，一旦有消息就直接发送给对端客户端
+func(u *User)ListenMessage(){
+
+	for {
+		msg := <-u.UserChan
+		_, err := u.conn.Write([]byte(msg+"\n"))
+		if err != nil{
+			fmt.Println(err)
+		}
+	}
+
+}
